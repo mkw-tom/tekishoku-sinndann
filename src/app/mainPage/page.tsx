2b8dc999477ceff7c7fa8fed600f312eb8f84703
Tomu Maekawa <tomu.m1107@icloud.com>
2024-11-03 11:07:10 +0900

    add mainPage

---
+++ src/app/mainPage/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import LogoutButton from "./components/LogoutButton";
+
+const Page = () => {
+	return (
+		<div>
+			mainPage
+			<LogoutButton />
+		</div>
+	);
+};
+
+export default Page;
