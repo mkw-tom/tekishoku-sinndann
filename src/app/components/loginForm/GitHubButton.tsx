cd10249e0512e12e6cd576e824ff2f702f59329c
Tomu Maekawa <tomu.m1107@icloud.com>
2024-11-03 11:06:48 +0900

    edit buttons

--- src/app/components/loginForm/GitHubButton.tsx
+++ src/app/components/loginForm/GitHubButton.tsx
@@ -1,22 +1,22 @@
 "use client";
-import { type UserType, clearUser, setUser } from "@/redux/slices/auth";
-import type { storeType } from "@/redux/store";
-import { signIn, signOut, useSession } from "next-auth/react";
+// import { clearUser, setUser } from "@/lib/redux/slices/auth";
+// import type { storeType } from "@/lib/redux/store";
+import { signIn, useSession } from "next-auth/react";
 import type React from "react";
-import { useEffect } from "react";
+// import { useEffect } from "react";
 import { FaGithub } from "react-icons/fa";
-import { useDispatch, useSelector } from "react-redux";
+// import { useDispatch, useSelector } from "react-redux";
 
 const GitHubButton = () => {
-	const dispatch = useDispatch();
-	const user = useSelector((state: storeType) => state.auth);
+	// const dispatch = useDispatch();
+	// const user = useSelector((state: storeType) => state.auth);
 
 	const { data: session } = useSession();
 	console.log(session?.user);
-	console.log(user);
+	// console.log(user);
 
 	const handleLogin = async () => {
-		await signIn("github");
+		await signIn("github", { callbackUrl: "/mainPage" });
 
 		// dispatch(
 		//   setUser({
@@ -29,33 +29,28 @@ const GitHubButton = () => {
 		// );
 	};
 
-	useEffect(() => {
-		if (session?.user) {
-			dispatch(
-				setUser({
-					customId: "123",
-					username: session?.user.name as string,
-					password: "gange",
-					profilePicture: session?.user.image as string,
-					email: session?.user.email as string,
-				}),
-			);
-		} else {
-			dispatch(clearUser());
-		}
-	}, [session, dispatch]);
-
-	const handleLogout = async () => {
-		await signOut();
-
-		dispatch(clearUser());
-	};
+	// useEffect(() => {
+	// 	if (session?.user) {
+	// 		dispatch(
+	// 			setUser({
+	// 				customId: "123",
+	// 				username: session?.user.name as string,
+	// 				password: "gange",
+	// 				profilePicture: session?.user.image as string,
+	// 				email: session?.user.email as string,
+	// 			}),
+	// 		);
+	// 	} else {
+	// 		dispatch(clearUser());
+	// 		console.log(user)
+	// 	}
+	// }, [session, dispatch, user]);
 
 	return (
 		<button
 			type="button"
 			className="btn btn-xl w-full text-lg"
-			onClick={session?.user ? handleLogout : handleLogin}
+			onClick={handleLogin}
 		>
 			<FaGithub className="w-7 h-7 " />
 			{session?.user ? "ログアウト" : "GitHubで新規登録"}
