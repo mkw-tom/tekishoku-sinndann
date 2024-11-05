32d20964c199d561bd1a86e65730ec0b5c9d49e7
Tomu Maekawa <tomu.m1107@icloud.com>
2024-11-05 15:22:30 +0900

    add google signIn func

--- src/app/components/loginForm/GitHubButton.tsx
+++ src/app/components/loginForm/GitHubButton.tsx
@@ -1,50 +1,12 @@
 "use client";
-// import { clearUser, setUser } from "@/lib/redux/slices/auth";
-// import type { storeType } from "@/lib/redux/store";
-import { signIn, useSession } from "next-auth/react";
+import { signIn } from "next-auth/react";
 import type React from "react";
-// import { useEffect } from "react";
 import { FaGithub } from "react-icons/fa";
-// import { useDispatch, useSelector } from "react-redux";
 
 const GitHubButton = () => {
-	// const dispatch = useDispatch();
-	// const user = useSelector((state: storeType) => state.auth);
-
-	const { data: session } = useSession();
-	console.log(session?.user);
-	// console.log(user);
-
 	const handleLogin = async () => {
 		await signIn("github", { callbackUrl: "/mainPage" });
-
-		// dispatch(
-		//   setUser({
-		//     customId: "123",
-		//     username: session?.user.name as string,
-		//     password: "gange",
-		//     profilePicture: session?.user.image as string,
-		//     email: session?.user.email as string,
-		//   })
-		// );
 	};
-
-	// useEffect(() => {
-	// 	if (session?.user) {
-	// 		dispatch(
-	// 			setUser({
-	// 				customId: "123",
-	// 				username: session?.user.name as string,
-	// 				password: "gange",
-	// 				profilePicture: session?.user.image as string,
-	// 				email: session?.user.email as string,
-	// 			}),
-	// 		);
-	// 	} else {
-	// 		dispatch(clearUser());
-	// 		console.log(user)
-	// 	}
-	// }, [session, dispatch, user]);
 
 	return (
 		<button
@@ -53,8 +15,7 @@ const GitHubButton = () => {
 			onClick={handleLogin}
 		>
 			<FaGithub className="w-7 h-7 " />
-			{session?.user ? "ログアウト" : "GitHubで新規登録"}
-			{/* GitHubで新規登録 */}
+			GitHubで新規登録
 		</button>
 	);
 };
--- src/app/components/loginForm/GoogleButton.tsx
+++ src/app/components/loginForm/GoogleButton.tsx
@@ -1,10 +1,18 @@
 "use client";
+import { signIn } from "next-auth/react";
 import React from "react";
 import { FcGoogle } from "react-icons/fc";
 
 const GoogleButton = () => {
+	const handleLogin = async () => {
+		await signIn("github", { callbackUrl: "/mainPage" });
+	};
 	return (
-		<button type="button" className="btn  btn-xl w-full text-lg">
+		<button
+			type="button"
+			className="btn  btn-xl w-full text-lg"
+			onClick={handleLogin}
+		>
 			<FcGoogle className="w-7 h-7" />
 			Googleで新規登録
 		</button>
