f0bf1d457ee515c55ca2fb795cd0d772be014494
Tomu Maekawa <tomu.m1107@icloud.com>
2024-11-06 23:40:32 +0900

    set user profile image

--- src/app/components/UserProf.tsx
+++ src/app/components/UserProf.tsx
@@ -8,20 +8,19 @@ const UserProf = () => {
 	return (
 		<div className="w-full relative group">
 			<img
-				src={(user?.profilePicture as string) || ""}
+				src={(user?.profilePicture as string) || "/default_icon.png"}
 				// biome-ignore lint/a11y/noRedundantAlt: <explanation>
 				alt={"Users profile picture"}
-				className="w-12 h-12 rounded-full border-2 border-base-100 "
+				className="w-12 h-12 rounded-full border-2 border-base-100 shadow-xl"
 			/>
-			<div className="hidden bg-base-200 w-96 h-20 absolute right-1 -top-2 rounded-md group-hover:flex items-center px-5 gap-2 ">
+			<div className="hidden bg-base-200 w-72 h-20 absolute right-1 -top-2 rounded-md group-hover:flex items-center px-5 gap-2 shadow-xl">
 				<img
 					src={(user?.profilePicture as string) || ""}
 					// biome-ignore lint/a11y/noRedundantAlt: <explanation>
 					alt="User's profile picture"
-					className="w-12 h-12 rounded-full border-2 border-base-100 "
+					className="w-12 h-12 rounded-full  "
 				/>
 				<h3 className="text-xl">{user.username}</h3>
-				<p className="text-md ml-auto">性格タイプ：INTJ</p>
 			</div>
 		</div>
 	);
