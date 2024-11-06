947625d45c4a42a91f6493fff73522bd806282e5
Tomu Maekawa <tomu.m1107@icloud.com>
2024-11-06 23:42:27 +0900

    create login toggle action

--- src/app/components/loginForm/GitHubButton.tsx
+++ src/app/components/loginForm/GitHubButton.tsx
@@ -1,9 +1,12 @@
 "use client";
+import type { storeType } from "@/app/types/ReduxTypes";
 import { signIn } from "next-auth/react";
 import type React from "react";
 import { FaGithub } from "react-icons/fa";
+import { useSelector } from "react-redux";
 
 const GitHubButton = () => {
+	const login = useSelector((state: storeType) => state.loginToggle);
 	const handleLogin = async () => {
 		await signIn("github", { callbackUrl: "/mainPage" });
 	};
@@ -15,7 +18,7 @@ const GitHubButton = () => {
 			onClick={handleLogin}
 		>
 			<FaGithub className="w-7 h-7 " />
-			GitHubで新規登録
+			GitHubで{login.login ? "ログイン" : "新規登録"}
 		</button>
 	);
 };
--- src/app/components/loginForm/LoginForm.tsx
+++ src/app/components/loginForm/LoginForm.tsx
@@ -2,18 +2,12 @@ import React from "react";
 import EmailIForm from "./EmailForm";
 import GitHubButton from "./GitHubButton";
 import GoogleButton from "./GoogleButton";
+import LoginSwitch from "./LoginSwitch";
 
 const LoginForm = () => {
 	return (
 		<section className="flex flex-col gap-10 w-4/12 h-lvh bg-base-100 bg-opacity-60 ">
-			<div className="text-xl text-center mt-20  font-bold ">
-				<button type="button" className="border-b-4 px-3 border-info text-info">
-					新規登録
-				</button>
-				<button type="button" className=" px-3 ">
-					ログイン
-				</button>
-			</div>
+			<LoginSwitch />
 			<div className="flex flex-col gap-3">
 				<div className="w-7/12 mx-auto">
 					<GoogleButton />
