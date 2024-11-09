78249f59a5e2f25339296b7bad9a6e9d722a3600
Tomu Maekawa <tomu.m1107@icloud.com>
2024-11-09 16:54:06 +0900

    update responsive logo

--- src/app/components/RightView.tsx
+++ src/app/components/RightView.tsx
@@ -8,11 +8,11 @@ const RightView = () => {
 		<section className="w-full lg:w-8/12 h-lvh mx-auto ">
 			<div className="flex flex-col w-11/12 my-20 gap-10 mx-auto">
 				<div className="text-center py-2 w-full mx-auto">
-					<h1 className="text-4xl md:text-6xl font-mono tracking-wider mx-auto font-bold text-white flex items-center justify-center gap-5">
+					<h1 className="text-4xl md:text-6xl font-mono tracking-wider mx-auto font-bold text-white flex items-center justify-center gap-3 md:gap-5">
 						<img
 							src="/file.png"
 							alt="ロゴ"
-							className="w-12 h-12 border-2 rounded-md border-white"
+							className="size-8 md:size-12 border-2 rounded-md border-white"
 						/>
 						<div>
 							<span className="">Work</span>
