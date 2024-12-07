8d1254781b8b8a926e008ae11937faf77a7ad49a
Tomu Maekawa <tomu.m1107@icloud.com>
2024-12-07 15:05:54 +0900

    add loadingUI

---
+++ src/app/loading.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./components/Header";
+
+const loading = () => {
+	return (
+		<div className="h-lvh  bg-gradient-to-tr to-info from-success">
+			<div className="mx-auto flex h-full w-28 flex-col items-center justify-center lg:text-xl">
+				<span className="font-bold tracking-wider text-base-100 dark:text-info">
+					Loading
+				</span>
+				{/* biome-ignore lint/style/useSelfClosingElements: <explanation> */}
+				<span className="loading loading-bars loading-lg text-base-100  dark:text-info"></span>
+			</div>
+		</div>
+	);
+};
+
+export default loading;
